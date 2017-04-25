5446b1861deb92aeabaaaa13f8bb9d8dbf98f28b
Nicolás Stefoni <nicolas.stefoni@ecloudsolutions.com>
2017-04-25 16:59:02 -0300

    Initial commit

---
+++ Example/SwiftCore/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftCore
+//
+//  Created by Nicolás Stefoni on 04/25/2017.
+//  Copyright (c) 2017 Nicolás Stefoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
